d806cff8495defe54f665ff521e2c851bbe12382
BlueCl0wn <darek092003@gmail.com>
2023-03-03 20:49:32 +0100

    7 inhaltlich vorbereitet

--- Versuch 7.py
+++ Versuch 7.py
@@ -1,13 +1,37 @@
 from function import *
-n = [12, 20, 24, 22]
+
+# 1. Beugung am Spalt -------------------------
+# 1.)
+print("1.1.)")
+
+# l : lambda
+l = [517.30844, 574.095682, 600.9334889, 615.25405749]
+wert_x(l, name="lambda")
+
+# 2.)
+n = [12, 20, 24, 22]  # n Grad
 c = [0.133, 0.123, 0.103, 0.0725]
 
+# 2.)
+
 def x(n, c) -> list:
-    return [(2*n_i)/(c_i) for n_i, c_i in zip(n, c)]
+    """
+    x-Achsenwerte nach Gleichung 7.6
+    :param n:
+    :param c:
+    :return:
+    """
+    return [(2 * n_i) / (c_i) for n_i, c_i in zip(n, c)]
 
-x_list = x(n, c) # x Werte berechnet nach 7.6 zum plotten
 
-y_0 = [80, 160, 240, 320] # Spaltbreite in mikrometer
-y = [round(i*10**-6, 15) for i in y_0] # Spaltbreite in Meterns
+x_list = x(n, c)  # x Werte berechnet nach 7.6 zum plotten
 
-wert_xy("lambda", x_list, y)
+y_mikrom = [80, 160, 240, 320]  # Spaltbreite in mikrometer
+y_meter = [round(i * 10 ** -6, 15) for i in y_mikrom]  # Spaltbreite in Meterns
+
+wert_xy( x_list, y_meter, name="lambda")
+
+
+lambd, y_0 = wert_xy(x_list, y_mikrom, name="lambda")
+
+graph(x_list, y_mikrom, trendlinie=True, title="Test", xlabel=r"$x_i$ in $1/\mu m$", ylabel=r"$y_{i}$ in $\mu m$")
--- function.py
+++ function.py
@@ -1,4 +1,9 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
 def mean(l: list) -> int:
     return sum(l) / len(l)
 
@@ -60,24 +65,48 @@ def get_s_a(x, y):
     return math.sqrt(s)
 
 
-def wert_x(name: str, x: list) -> None:
-    print(f"{name}:")
+def wert_x(x: list, name: str = None) -> int:
     x_mean = round(mean(x), 9)
     s_x_mean = round(get_s_x(x), 9)
-    perc = round(s_x_mean/x_mean, 6)
-    print(f" -  {name}_mean = {x_mean} +- {s_x_mean}    (+- {perc})")
+    perc = round(s_x_mean / x_mean, 6)
+    print(f"{name}_mean = {x_mean} +- {s_x_mean}    (+- {perc})")
     print()
+    return x_mean
 
 
-def wert_xy(name: str, x: list, y: list) -> None:
+def wert_xy(x: list, y: list, name: str = None) -> tuple:
     print(f"{name}:")
-    b=round(get_b(x, y), 9)
+    b = round(get_b(x, y), 9)
     s_b = round(get_s_b(x, y), 9)
-    b_perc = round(s_b/b, 6)
+    b_perc = round(s_b / b, 6)
 
     a = round(get_a(x, y), 9)
     s_a = round(get_s_a(x, y), 9)
-    a_perc = round(s_a/a, 6)
+    a_perc = round(s_a / a, 6)
     print(f" -  b = {b} +- {s_b}  (+- {b_perc})")
     print(f" -  a = {a} +- {s_a}  (+- {a_perc})")
     print()
+
+    return b, a
+
+
+def get_Trendlinie(x: list, y: list):
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    return p(x)
+
+
+def graph(x: list, y: list, trendlinie=False, title=None, xlabel=None, ylabel=None, text_pos=(300, 250)):
+    fig, ax = plt.subplots(layout='constrained')
+
+    if trendlinie:
+        b, a = wert_xy(x, y)
+        ax.plot(x, [(b * i + a) for i in x], color="grey", linestyle="dashed")
+        ax.text(text_pos[0], text_pos[1], rf"{round(b, 3)}$*x + {round(a, 3)}$")
+
+    ax.scatter(x, y, linewidths=2)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.grid()
+    plt.show()
