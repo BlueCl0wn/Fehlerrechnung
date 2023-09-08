07b92555241861d2a529e322994e1fe6b1d8a21f
BlueCl0wn <darek092003@gmail.com>
2023-09-08 08:47:23 +0200

    dssdf

--- Prakt2/Versuch M4.py
+++ Prakt2/Versuch M4.py
@@ -1,31 +1,33 @@
 from function import *
 import numpy as np
 
+phi_e = 5
+
 # ------ Teil 1 ---------
 maxima_t = [0.62, 2.59, 4.55, 6.55, 8.52, 10.48, 12.47, 14.45]
-maxima_phi =  [24.8, 18.3, 12.8, 9.3, 6.3, 4.3, 3.3, 1.8]
+maxima_phi = [24.8, 18.3, 12.8, 9.3, 6.3, 4.3, 3.3, 1.8]
 
 b, a, s_b, s_a = wert_xy(maxima_t, maxima_phi, "Teil 1")
 delta_1 = b
 
 omega_1 = 3.173
-omega_0_1 = np.sqrt(omega_1**2 + delta**2)
+omega_0_1 = np.sqrt(omega_1 ** 2 + delta_1 ** 2)
 
 
 # -------- Teil 2 -----
 
-omega_0 = None
-phi_e = None
 
-
-def phi_erz(omega_e: int) -> int:
+def phi_erz(omega_e: int, omega_0, phi_e) -> int:
     """
     Berechent theoretische Amplitudenresonanzkurve
-    :param omega_e:
+    :param phi_e: Ampltiude des Erregers
+    :param omega_0: ungedämpftes Omega
+    :param omega_e: Omega des Erregers
     :return:
     """
-    return omega_0**2 / np.sqrt((omega_0**2 - omega_e**2)**2 + 4 * delta**2 * omega_e**2) * phi_e
+    return omega_0 ** 2 / np.sqrt((omega_0 ** 2 - omega_e ** 2) ** 2 + 4 * delta_1 ** 2 * omega_e ** 2) * phi_e
+
 
 omegas = np.linspace(0.2, 1.4, 1000)
-y_ampkurve = phi_erz(omegas)#
+y_ampkurve = phi_erz(omegas, omega_0_1, phi_e)
 graph(omegas, y_ampkurve, title="Amplitudenresonanzkurve", xlabel=r"\omega", ylabel=r"Amplitude")
